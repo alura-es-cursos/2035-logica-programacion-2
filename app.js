81ac13e48a1661b27e07eaa91b9bdee8c1e1d03a
Leonardo Jose <ljcl79@gmail.com>
2023-09-11 08:00:39 -0300

    Aula2

---
+++ app.js
@@ -0,0 +1,28 @@
+let numeroSecreto = generarNumeroSecreto();
+let intentos = 0;
+
+console.log(numeroSecreto);
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        console.log('Acertaste el número!');
+    } 
+    return;
+}
+
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
+
+}
+
+asignarTextoElemento('h1','Juego del número secreto!');
+asignarTextoElemento('p',`Indica un número del 1 al 10`);
